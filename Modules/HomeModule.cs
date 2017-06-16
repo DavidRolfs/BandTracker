d4fae7a3669877f369428a61427c22b29b2e9c24
david rolfs <rolfs97@yahoo.com>
2017-06-16 11:58:47 -0700

    add home page in module

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace BandTracker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/bands"] = _ => {
+        List<Band> AllBands = Band.GetAll();
+        return View["bands.cshtml", AllBands];
+      };
+      Get["/venues"] = _ => {
+        List<Venue> AllVenues = Venue.GetAll();
+        return View["venues.cshtml", AllVenues];
+      };
+    }
+  }
+}
